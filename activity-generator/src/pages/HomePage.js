1116b0c376bdb89e42029ceab3dd713ca3fdce04
Stefan <sn1319@ic.ac.uk>
2021-03-27 23:18:27 +0000

    added scopes inside the google login api for reading contacts and reading and writing to the calendar

--- activity-generator/src/pages/HomePage.js
+++ activity-generator/src/pages/HomePage.js
@@ -4,9 +4,26 @@ import { Component } from 'react';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 import './HomePage.css';
 
+var accessToken;
+
+function test() {
+  fetch('https://www.google.com/m8/feeds/contacts/default/full', {
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
+}
+
 export class HomePage extends Component {
 
   responseGoogle = (response) => {
+    console.log(response);
+    accessToken = response.accessToken;
+  }
+
+  responseContacts = (response) => {
     console.log(response);
   }
 
@@ -18,11 +35,13 @@ export class HomePage extends Component {
             <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
         <GoogleLogin
           clientId = { process.env.REACT_APP_GOOGLE_SIGNIN_KEY }
+          scope = "profile email https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/calendar"
           buttonText="Login"
           onSuccess={this.responseGoogle}
           onFailure={this.responseGoogle}
           cookiePolicy={'single_host_origin'}
         />
+        <button onClick={test}>Meh</button>
       </div>
     )
   }
