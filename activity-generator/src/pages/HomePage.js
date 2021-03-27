941b4abc26fa920f5ae4eb55f2c0ccbed923e254
P0trak <81471530+P0trak@users.noreply.github.com>
2021-03-27 18:18:47 +0000

    Update HomePage.js

--- activity-generator/src/pages/HomePage.js
+++ activity-generator/src/pages/HomePage.js
@@ -9,5 +9,5 @@ export function HomePage() {
             <h2>Lorem Ipsum</h2>
             <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
         </div>
-    )
+    </div>)
 }
