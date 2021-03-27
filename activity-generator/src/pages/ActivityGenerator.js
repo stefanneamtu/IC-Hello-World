ee7c9e90e88c164bd66dc1c30d8b13199da10daa
Christina Chin <christina.chin95@gmail.com>
2021-03-27 19:39:18 +0000

    random image for random activity

--- activity-generator/src/pages/ActivityGenerator.js
+++ activity-generator/src/pages/ActivityGenerator.js
@@ -1,7 +1,11 @@
+import {createApi} from 'unsplash-js'
+import {useState} from 'react'
+
 export function ActivityGenerator(){
   
     var activities = ["Zoom call", "Walk in park", "Quiz"];
-    var randActivity;
+    var randActivity = randomActivity();
+    const [imgUrl, setUrl] = useState(null);
 
     function showWheel(){
         <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
@@ -10,6 +14,21 @@ export function ActivityGenerator(){
 
     function randomActivity(){
         randActivity = activities[Math.floor(Math.random() * activities.length)];
+        var queryString = randActivity.concat(" Home")
+        console.log(randActivity)
+        
+        getImage(queryString) 
+        return randActivity
+    }
+
+    function getImage(queryString) {
+        const unsplash = createApi({
+            accessKey: 'TXmsxWk4HiObYTj_WKQ-2jnB3wRe28pqV2MWrMDrdeY'
+        })
+        var response = unsplash.search.getPhotos({
+            query: queryString
+        })
+        console.log(response)
     }
 
     return(
@@ -17,6 +36,8 @@ export function ActivityGenerator(){
         <h1>activity generator</h1>
         <button onClick={showWheel}>Pick me an activity</button>
         <p>{randActivity}</p>
+        <img src={"https://images.unsplash.com/photo-1467238307002-480ffdd260f2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTgzODN8MHwxfHNlYXJjaHwyfHxXYWxrJTIwaW4lMjBwYXJrJTIwSG9tZXxlbnwwfHx8fDE2MTY4NzI0MTE&ixlib=rb-1.2.1&q=80&w=1080"} alr = "loading..." />        
+        
       </div>
     );
 }
