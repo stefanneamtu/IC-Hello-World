e957cf5cb5b8fbe32fa197094efd37ba67f410f6
Stefan <sn1319@ic.ac.uk>
2021-03-27 17:22:27 +0000

    added react-google-login api, created basic login with google account in the HomePage component and hid the API key in the .env file

--- activity-generator/src/App.js
+++ activity-generator/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import {HomePage} from './pages/HomePage'
+import { HomePage } from './pages/HomePage'
 
 function App() {
   return (
-    <HomePage />
-  );
+    <div>
+      <HomePage />
+    </div>
+  )
 }
 
 export default App;
--- activity-generator/src/pages/HomePage.js
+++ activity-generator/src/pages/HomePage.js
@@ -1,7 +1,24 @@
-export function HomePage() {
-    return(
-        <div>
-            <h1>Activity Generator</h1>
-        </div>
+import GoogleLogin from 'react-google-login'
+import { Component } from 'react';
+
+export class HomePage extends Component {
+
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Activity Generator</h1>
+        <GoogleLogin
+          clientId = { process.env.REACT_APP_GOOGLE_SIGNIN_KEY }
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+      </div>
     )
+  }
 }
