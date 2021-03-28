d78a7a86ad787cb7982f662f69ebe3c3e6e81b72
Patrick Lotery <pdl119@ic.ac.uk>
2021-03-28 00:16:28 +0000

    Moved adding to calendar to the ActivityGenerator page but needs better way to access credentials

--- activity-generator/src/pages/ActivityGenerator.js
+++ activity-generator/src/pages/ActivityGenerator.js
@@ -1,12 +1,147 @@
+import './ActivityGenerator.css';
+import Button from 'react-bootstrap/Button';
+import {createApi} from 'unsplash-js'
+import React, {useState} from 'react'
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import ReactDOM from 'react-dom';
+
 export function ActivityGenerator() {
 
     var activities = ["Zoom call", "Walk in park", "Quiz"];
-    var randActivity = activities[Math.floor(Math.random() * activities.length)];
+    var randActivity = randomActivity();
+    const [imgUrl, setUrl] = useState(null);
+
+
+    function showWheel(){
+        <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
+        randActivity = randomActivity();
+
+        const element = (
+            /*<div>
+            <div>
+            <ul>
+                <li>
+                <Link to="/"> <p2>Home</p2></Link>
+                </li>
+                <li>
+                <Link to="/activity"> <p2>Random activity generator</p2></Link>
+                </li>
+            </ul>
+            </div>*/
+            <div class="wrapper">
+            <h1>activity generator</h1>
+            <button class="button" onClick={showWheel}>Pick me an activity</button>
+            <p>{randActivity}</p>
+            <button class="button" onClick={addEvent}>Add to my calendar</button>
+            <img src={"imgUrl"} alt = "loading..." />        
+            
+            </div>
+            //</div>
+        )
+
+        ReactDOM.render(element, document.getElementById('root'));
+    }
+
+    function randomActivity(){
+        randActivity = activities[Math.floor(Math.random() * activities.length)];
+        var queryString = randActivity.concat(" Home")
+        console.log(randActivity)
+        
+        console.log(queryString)
+        getImage(queryString) 
+        return randActivity
+    }
+
+    function getImage(queryString) {
+        const unsplash = createApi({
+            accessKey: 'TXmsxWk4HiObYTj_WKQ-2jnB3wRe28pqV2MWrMDrdeY'
+        })
+        var response = unsplash.search.getPhotos({
+            query: queryString
+        }).then((response) => {
+            console.log(response)
+            // setUrl(response["results"][0]["urls"]["regular"])
+        })
+        
+    }
+
+    function addEvent() {
+        const {google} = require('googleapis');
+        const fs = require('fs');
+        var credentials;
+
+        fs.readFile('credentials.json', (err, content) => {
+            if (err) return console.log('Error loading client secret file:', err);
+            // Authorize a client with credentials, then call the Google Calendar API.
+            
+            credentials = JSON.parse(content);
+            
+        });
+        console.log("line 78");
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const auth = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+        //adds an event to my calendar
+        
+        const calendar = google.calendar({version: 'v3', auth});
+        
+        // Refer to the Node.js quickstart on how to setup the environment:
+        // https://developers.google.com/calendar/quickstart/node
+        // Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+        // stored credentials.
+
+        var event = {
+            'summary': 'Test event',
+            'description': 'Can I add events to my calendar?',
+            'start': {
+            'dateTime': '2021-03-27T22:00:00+00:00',
+            },
+            'end': {
+            'dateTime': '2021-03-27T23:00:00+00:00',
+            },
+            'reminders': {
+            'useDefault': false,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},
+                {'method': 'popup', 'minutes': 10},
+            ],
+            },
+        };
+        
+        calendar.events.insert({
+            auth: auth,
+            calendarId: 'primary',
+            resource: event,
+        }, function(err, event) {
+            if (err) {
+            console.log('There was an error contacting the Calendar service: ' + err);
+            return;
+            }
+            console.log('Event created: %s', event.htmlLink);
+        });
+    }
 
     return(
+        /*<div>
         <div>
-            <h1>Activity Generator</h1>
+        <ul>
+            <li>
+            <Link to="/"> <p2>Home</p2></Link>
+            </li>
+            <li>
+            <Link to="/activity"> <p2>Random activity generator</p2></Link>
+            </li>
+        </ul>
+        </div>*/
+        <div class="wrapper">
+            <h1> Random activity generator</h1>
+            <button class="button" onClick={showWheel}>Pick me an activity</button>
             <p>{randActivity}</p>
+            <button class="button" onClick={addEvent}>Add to my calendar</button>
+            <img src={"imgUrl"} alt = "loading..." />        
+        
         </div>
-    )
+        //</div>
+    );
 }
--- activity-generator/src/pages/HomePage.js
+++ activity-generator/src/pages/HomePage.js
@@ -1,7 +1,61 @@
+<<<<<<< Updated upstream
 export function HomePage() {
     return(<div>
         <div>
             <p>Sign in with your Google account (to get your calendar)</p>
         </div>
     </div>)
+=======
+
+import GoogleLogin from 'react-google-login'
+import { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
+import './HomePage.css';
+
+var accessToken;
+
+function test() {
+  fetch('https://www.google.com/m8/feeds/contacts/default/full', {
+    headers: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+  .then(response => response.json())
+  .then(json => console.log(json));
+}
+
+
+
+export class HomePage extends Component {
+
+  responseGoogle = (response) => {
+    console.log(response);
+    accessToken = response.accessToken;
+  }
+
+  responseContacts = (response) => {
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>.keepintouch</h1>
+        <h2>Afraid of moving away from your friends?</h2>
+        <p>Try our new app that allows you to live random experiences and activities with your friends. Just sign in using your Google account and we will take care of the rest</p>
+        <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="wheel" class="center"/>
+        
+        <GoogleLogin
+          clientId = { process.env.REACT_APP_GOOGLE_SIGNIN_KEY }
+          scope = "profile email https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/calendar"
+          buttonText="Login"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
+        <button onClick={test}>Meh</button>
+      </div>
+    )
+  }
+>>>>>>> Stashed changes
 }
