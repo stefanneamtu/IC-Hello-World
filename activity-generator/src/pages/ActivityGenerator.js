bd0526db873419e4ecfc14f0ab39252f4bb05c3e
Stefan <sn1319@ic.ac.uk>
2021-03-28 12:29:42 +0100

    added random contacts generation

--- activity-generator/src/pages/ActivityGenerator.js
+++ activity-generator/src/pages/ActivityGenerator.js
@@ -1,65 +1,75 @@
 import './ActivityGenerator.css';
 import Button from 'react-bootstrap/Button';
-import {createApi} from 'unsplash-js'
-import React, {useState} from 'react'
+import { createApi } from 'unsplash-js'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
+import {contacts} from './HomePage';
 
 
-export function ActivityGenerator(){
-  
-    var activities = ["Zoom call", "Walk in park", "Quiz"];
-    var randActivity = randomActivity();
-    const [imgUrl, setUrl] = useState(null);
+export function ActivityGenerator() {
+
+  var activities = ["Zoom call", "Walk in park", "Quiz"];
+  var randActivity = randomActivity();
+  var randContact = randomContact();
+  const [imgUrl, setUrl] = useState(null);
 
 
-    function showWheel(){
-        <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
-        randActivity = randomActivity();
+  function showWheel() {
+    <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
+    randActivity = randomActivity();
+    randContact = randomContact();  
 
-        const element = (
-            <div class="wrapper">
-            <h1>activity generator</h1>
-            <button class="button" onClick={showWheel}>Pick me an activity</button>
-            <p>{randActivity}</p>
-            <img src={"imgUrl"} alt = "loading..." />        
-            
-            </div>
-        )
+    const element = (
+      <div class="wrapper">
+        <h1>activity generator</h1>
+        <button class="button" onClick={showWheel}>Pick me an activity</button>
+        <p>{randActivity} with {randContact}!</p>
+        <img src={"imgUrl"} lt="loading..." />
 
-        ReactDOM.render(element, document.getElementById('root'));
-    }
+      </div>
+    )
 
-    function randomActivity(){
-        randActivity = activities[Math.floor(Math.random() * activities.length)];
-        var queryString = randActivity.concat(" Home")
-        console.log(randActivity)
-        
-        console.log(queryString)
-        getImage(queryString) 
-        return randActivity
-    }
+    ReactDOM.render(element, document.getElementById('root'));
+  }
 
-    function getImage(queryString) {
-        const unsplash = createApi({
-            accessKey: 'TXmsxWk4HiObYTj_WKQ-2jnB3wRe28pqV2MWrMDrdeY'
-        })
-        var response = unsplash.search.getPhotos({
-            query: queryString
-        }).then((response) => {
-            console.log(response)
-            // setUrl(response["results"][0]["urls"]["regular"])
-        })
-        
-    }
+  function randomActivity() {
+    randActivity = activities[Math.floor(Math.random() * activities.length)];
+    var queryString = randActivity.concat(" Home")
+    // console.log(randActivity)
 
-    return(
-        <div class="wrapper">
-            <h1> Random activity generator</h1>
-            <button class="button" onClick={showWheel}>Pick me an activity</button>
-            <p>{randActivity}</p>
-            <img src={"imgUrl"} alt = "loading..." />        
-        
+    // console.log(queryString)
+    getImage(queryString)
+    return randActivity
+  }
 
-        </div>
-    );
+  function randomContact() {
+    randContact = contacts[Math.floor(Math.random() * contacts.length)];
+    console.log(contacts);
+    console.log(contacts[0]);
+    return randContact;
+  }
+
+  function getImage(queryString) {
+    const unsplash = createApi({
+      accessKey: 'TXmsxWk4HiObYTj_WKQ-2jnB3wRe28pqV2MWrMDrdeY'
+    })
+    var response = unsplash.search.getPhotos({
+      query: queryString
+    }).then((response) => {
+      console.log(response)
+      // setUrl(response["results"][0]["urls"]["regular"])
+    })
+
+  }
+
+  return (
+    <div class="wrapper">
+      <h1> Random activity generator</h1>
+      <button class="button" onClick={showWheel}>Pick me an activity</button>
+      <p>{randActivity}</p>
+      <img src={"imgUrl"} alt="loading..." />
+
+
+    </div>
+  );
 }
--- activity-generator/src/pages/HomePage.js
+++ activity-generator/src/pages/HomePage.js
@@ -5,15 +5,25 @@ import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 import './HomePage.css';
 
 var accessToken;
+export var contacts = [];
 
-function test() {
+function importContacts() {
   fetch('https://www.google.com/m8/feeds/contacts/default/full', {
     headers: {
       'Authorization': 'Bearer ' + accessToken
     }
   })
-  .then(response => response.json())
-  .then(json => console.log(json));
+  .then(response => response.text())
+  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
+  .then(data => {
+    contacts = [];
+    for (const contact of data.getElementsByTagName("entry")) {
+      const contactTitle = contact.getElementsByTagName("title")[0].childNodes;
+      if (contactTitle.length > 0) {
+        contacts.push(contactTitle[0].textContent);
+      }
+    }
+  });
 }
 
 export class HomePage extends Component {
@@ -23,18 +33,12 @@ export class HomePage extends Component {
     accessToken = response.accessToken;
   }
 
-  responseContacts = (response) => {
-    console.log(response);
-  }
-
   render() {
     return (
       <div>
         <h1>.keepintouch</h1>
-        <h2>Affraid of moving away from your friends ?</h2>
-        <p>Try our new app that allows you to live random experiences and activities with your friends. Just sign in using your Google account and we will take care of the rest</p>
-        <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="wheel" class="center"/>
-        
+            <h2>Lorem Ipsum</h2>
+            <img src={"https://i.pinimg.com/originals/94/cc/d5/94ccd56f2a24d1eb9486d86fcee0b3b1.gif"} alt="loading..." />
         <GoogleLogin
           clientId = { process.env.REACT_APP_GOOGLE_SIGNIN_KEY }
           scope = "profile email https://www.googleapis.com/auth/contacts.readonly https://www.googleapis.com/auth/calendar"
@@ -43,7 +47,7 @@ export class HomePage extends Component {
           onFailure={this.responseGoogle}
           cookiePolicy={'single_host_origin'}
         />
-        <button onClick={test}>Meh</button>
+        <button onClick={importContacts}>Import Contacts</button>
       </div>
     )
   }
